9303f12a4147554d9fc089e871e8a3384cd615c9
Nick Young <mycorpweb@gmail.com>
2013-12-23 14:17:40 -0800

    Added a new functions.php file and empty functions that will be used for getting the code for each widget type

---
+++ pinterest-widgets/admin/includes/functions.php
@@ -0,0 +1,27 @@
+<?php
+
+
+// Returns the code needed for the Follow Button
+// Based on Pinterest code on 12/23/2012
+function pw_follow_button( ) {
+	
+}
+
+// Returns the code needed for the Pin Widget
+// Based on Pinterest code on 12/23/2012
+function pw_pin_widget() {
+	
+}
+
+// Returns the code needed for the Profile Widget
+// Based on Pinterest code on 12/23/2012
+function pw_profile_widget() {
+	
+}
+
+// Returns the code needed for the Board Widget
+// Based on Pinterest code on 12/23/2012
+function pw_board_widget() {
+	
+}
+
