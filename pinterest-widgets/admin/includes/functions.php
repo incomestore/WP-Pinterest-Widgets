6cc97d1ed3b20a7084ed687f81a071bb896f2b2a
Nick Young <mycorpweb@gmail.com>
2013-12-26 12:35:42 -0800

    Updated code functions.php added in code for board widget

--- pinterest-widgets/admin/includes/functions.php
+++ pinterest-widgets/admin/includes/functions.php
@@ -49,7 +49,28 @@ function pw_profile_widget( $url, $size ) {
 
 // Returns the code needed for the Board Widget
 // Based on Pinterest code on 12/23/2012
-function pw_board_widget() {
+function pw_board_widget( $url, $size ) {
+	// Default size options based on option "Square" from Pinterest
+	$scale_width  = 80;
+	$scale_height = 320;
+	$board_width  = 400;
 	
+	// Change sizes if sidebar option is selected
+	if( $size == 'sidebar' ) {
+		$scale_width  = 60;
+		$scale_height = 800;
+		$board_width  = 150;
+	}
+	
+	// Change sizes if header option is selected
+	if( $size == 'header' ) {
+		$scale_width  = 115;
+		$scale_height = 120;
+		$board_width  = 900;
+	}
+	
+	$board_widget = '<a data-pin-do="embedBoard" href="' . esc_attr( $url ) . '" data-pin-scale-width="' . $scale_width . '" data-pin-scale-height="' . $scale_height . '" data-pin-board-width="' . $board_width . '"></a>';
+	
+	return $board_widget;
 }
 
