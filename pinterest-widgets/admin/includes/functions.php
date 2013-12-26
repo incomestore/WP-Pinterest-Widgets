7bb866be94c9c90d4ebc0807c1b689dded5de63d
Nick Young <mycorpweb@gmail.com>
2013-12-26 11:58:09 -0800

    Added reusable code to output Profile Widget

--- pinterest-widgets/admin/includes/functions.php
+++ pinterest-widgets/admin/includes/functions.php
@@ -21,8 +21,30 @@ function pw_pin_widget( $url ) {
 
 // Returns the code needed for the Profile Widget
 // Based on Pinterest code on 12/23/2012
-function pw_profile_widget() {
+function pw_profile_widget( $url, $size ) {
 	
+	// Default size options based on option "Square" from Pinterest
+	$scale_width  = 80;
+	$scale_height = 320;
+	$board_width  = 400;
+	
+	// Change sizes if sidebar option is selected
+	if( $size == 'sidebar' ) {
+		$scale_width  = 60;
+		$scale_height = 800;
+		$board_width  = 150;
+	}
+	
+	// Change sizes if header option is selected
+	if( $size == 'header' ) {
+		$scale_width  = 115;
+		$scale_height = 120;
+		$board_width  = 900;
+	}
+	
+	$profile_widget = '<a data-pin-do="embedUser" href="' . esc_attr( $url ) . '" data-pin-scale-width="' . $scale_width . '" data-pin-scale-height="' . $scale_height . '" data-pin-board-width="' . $board_width . '"></a>';
+	
+	return $profile_widget;
 }
 
 // Returns the code needed for the Board Widget
